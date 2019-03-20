94046205d5f575820aacc466e9db3f37fdabd9ea
chong516 <36216342+chong516@users.noreply.github.com>
2019-03-20 17:01:54 +0900

    Add files via upload

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+
+def run_server(port=4000):
+    host = ''
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen(1)
+        conn, addr = s.accept()
+        msg = conn.recv(1024)
+        print(f'{msg.decode()}')
+        conn.sendall(msg)
+        conn.close()
+
+if __name__ == '__main__':
+    run_server()
